eed27a41204c8cc1b7bcf2266aa1a280b68b9958
Colindy <cnellinger@yahoo.com>
2023-03-24 21:31:21 -0400

    I think I'm starting to get the hang of Classes and how to work with them.

--- user.py
+++ user.py
@@ -5,18 +5,29 @@ class User:
         self.lname = lname
         self.id = id
         self.department = department
+        self.login_attempts = 0
 
     def full_name(self):
-        print(f"User's full name is {self.fname} {self.lname}.")
+        print(f"User's full name is {self.fname} {self.lname}.\n")
 
     def who(self):
-        print(f"{self.fname}'s ID number is {self.id}.")
+        print(f"{self.fname}'s ID number is {self.id}.\n")
 
     def dept(self):
-        print(f"{self.fname} works in the {self.department} department.")
+        print(f"{self.fname} works in the {self.department} department.\n")
 
     def greet(self):
-        print(f"Welcome {self.fname} {self.lname}!  Good to see you!") 
+        print(f"Welcome {self.fname} {self.lname}!  Good to see you!\n")
+        
+    def increment_login_attempts(self, total_attempts):
+        self.login_attempts += total_attempts
+        print("Your login attempt has been recorded")
+        print(f"This is attempt number {self.login_attempts}.\n")
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+        print(f"Your login attempts counter has been reset to {self.login_attempts}.")
+
 
 """Now I have my class, let's make some users"""
 
@@ -30,6 +41,11 @@ user4 = User("Jeff", "Hardy", "489456", "High Flying Cool Brother")
 user1.full_name()
 user1.dept()
 user1.greet()
+print(user1.login_attempts)
+user1.increment_login_attempts(1)
+print(user1.login_attempts)
+user1.reset_login_attempts()
+print(user1.login_attempts)
 user2.full_name()
 user2.greet()
 user3.full_name()

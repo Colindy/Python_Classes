4d060566a893ef922edda95b4e9c6393414c5272
cnell <cnellinger@yahoo.com>
2023-03-28 20:07:50 -0400

    child classes in car.py

--- car.py
+++ car.py
@@ -30,22 +30,41 @@ class Car:
         """Adds the given amount to the odometer"""
         self.odometer += miles
 
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles"""
 
+    def __init__(self, make, model, year):
+        """
+        Initialize attributes of the parent class
+        Then initialize attributes of the child class
+        """
+        super().__init__(make, model, year)
+        self.battery_size = 75
+
+    def describe_battery(self):
+        """Print a statement describing the battery"""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+
+
+my_tesla = ElectricCar('tesla', 'model s', 2019)
 my_new_car = Car('toyota', 'prius', '2012')
 my_used_car = Car('chevy', 's10', '1985')
 print(my_new_car.get_discriptive_name())
-
+print(my_tesla.get_discriptive_name())
+my_tesla.describe_battery()
+print("\n")
 #my_new_car.odometer = 23  # Here we edit the odometer reading directly by assigning it a new value
 my_new_car.update_odometer(35)   # Here we use the module inside the Class to update the mileage
 my_new_car.read_odometer()
-
+print("\n")
 """Here we're going to check the 'rollback' check in update_odometer"""
 print(my_used_car.get_discriptive_name())
 my_used_car.update_odometer(285_835)
 my_used_car.read_odometer()
-
+print("\n")
 my_used_car.increment_odometer(500) # Here we increment the odometer
 my_used_car.read_odometer()
-
+print("\n")
 my_used_car.update_odometer(2_500) # Here we test the rollback check
 my_used_car.read_odometer()
+print("\n")

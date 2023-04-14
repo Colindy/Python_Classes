2588b50e16b1007f5177c51e27a1c91e11bf7506
cnell <cnellinger@yahoo.com>
2023-04-14 16:02:54 -0400

    some random fun ;)

---
+++ randostuff.py
@@ -0,0 +1,25 @@
+from random import randint, choice
+
+
+
+while True:
+    dice = randint(1, 6)
+    print(f"You rolled a {dice}!")
+
+    if dice == 3:
+        break
+
+while True:
+    dice = randint(1, 20)
+    print(f"You rolled a {dice}!")
+
+    if dice == 13:
+        break
+
+my_ticket = "a3e6f"
+
+# Create a loop to count iterations for how long it takes a random number to match my_ticket
+
+while True:
+    random
+
